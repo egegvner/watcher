8e700bacab8ea8fc9ea4664f076c8e612d978dba
Ege Güvener <93076687+egegvner@users.noreply.github.com>
2025-02-06 21:32:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import base64
 import sqlite3
 import streamlit_pdf_viewer
 import time
+import streamlit_pdf_viewer
+import pandas as pd
 
 PASSCODE = 1234
 
@@ -10,54 +12,73 @@ conn = sqlite3.connect("watcher.db", check_same_thread=False)
 c = conn.cursor()
 
 c.execute('''CREATE TABLE IF NOT EXISTS editions (
-          edition TEXT NOT NULL PRIMARY KEY,
+          edition_id INTEGER PRIMARY KEY NOT NULL,
+          edition TEXT NOT NULL,
           date TEXT NOT NULL,
-          file_data BLOB 
+          path BLOB 
           );''')
 conn.commit()
 
-@st.dialog(" ")
+@st.dialog("Admin")
 def access_admin_dialog():
-
-    st.subheader("📤 Upload a New PDF Edition", divider="rainbow")
-    uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
-    edition_number = st.text_input("Enter Edition Number")
-    date = st.text_input("Enter Date i.e. '1 Jan 2025'")
-
-    if st.button("Upload", use_container_width=True):
-        if uploaded_file and edition_number and date:
-            pdf_data = uploaded_file.read()
-            c.execute("INSERT INTO editions (edition, date, file_data) VALUES (?, ?, ?)", (edition_number, date, pdf_data))
-            conn.commit()
-            st.success(f"✅ Edition {edition_number} has been successfully uploaded!")
-            time.sleep(2)
+    guess = st.text_input("", label_visibility="collapsed", placeholder="Enter password")
+    if st.button("Enter", use_container_width=True):
+        if guess == str(PASSCODE):
+            st.session_state.admin = 1
             st.rerun()
-        else:
-            st.warning("⚠️ Please select a PDF and enter an edition number.")
 
 def fetch_pdf(edition):
-    c.execute("SELECT date, file_data FROM editions WHERE edition = ?", (edition,))
+    c.execute("SELECT date, path FROM editions WHERE edition = ?", (edition,))
     result = c.fetchone()
     return result if result else None
 
-c.execute("SELECT edition FROM editions")
-editions = [row[0] for row in c.fetchall()]
+def admin_view():
+    editions = c.execute("SELECT edition_id, edition, date, path FROM editions").fetchall()
+    if st.button("HOMEPAGE", use_container_width=True):
+        st.session_state.admin = 0
+        st.rerun()
+        
+    with st.expander("New Edition"):
+        edition = st.number_input("", label_visibility="collapsed", placeholder="Edition number", value=None)
+        date = st.text_input("", label_visibility="collapsed", placeholder="Date")
+        path = st.text_input("", label_visibility="collapsed", placeholder="Path i.e. './2024-November_December-25_1-035.pdf'")
+        if st.button("Upload New Edition", type="primary", use_container_width=True):
+            if edition and date and path:
+                c.execute("INSERT INTO editions (edition, date, path) VALUES (?, ?, ?)", (edition, date, path))
+                conn.commit()
 
-if st.button("Upload - Only for John"):
-    access_admin_dialog()
+    st.subheader("Editions", divider="rainbow")
+    df = pd.DataFrame(editions, columns = ["Edition ID","Edition Number", "Date", "File Path"])
+    edited_df = st.data_editor(df, key = "edition_table", num_rows = "fixed", use_container_width = True, hide_index = True)
+    if st.button("Update Editions", use_container_width = True):
+        for _, row in edited_df.iterrows():
+            c.execute("UPDATE OR IGNORE editions SET edition = ?, date = ?, path = ? WHERE edition_id = ?", (row["Edition"], row["Date"], row["File Path"], row["Edition ID"]))
+        conn.commit()
+        st.rerun()
 
 def main():
-    st.header("📖 Weekly Watcher Viewer", divider="rainbow")
-    st.subheader("📚 View an Edition")
-    edition = st.selectbox("Select an Edition", options=editions)
-    pdf_data = fetch_pdf(edition)
+    c.execute("DROP TABLE editions")
+    conn.commit()
+    if st.session_state.admin == 1:
+        admin_view()
+    else:
+        c.execute("SELECT edition FROM editions")
+        editions = [row[0] for row in c.fetchall()]
+        if st.button("Upload - Only for John"):
+            access_admin_dialog()
+        st.header("📖 Weekly Watcher Viewer", divider="rainbow")
+        st.subheader("📚 View an Edition")
+        edition = st.selectbox("Select an Edition", options=editions)
+        pdf_data = fetch_pdf(edition)
 
-    st.subheader(pdf_data[0], divider="rainbow")
-    with st.container(border=True):
-        if pdf_data:
-            streamlit_pdf_viewer.pdf_viewer(pdf_data[1], render_text=True)
-        else:
-            st.warning("⚠️ No PDF available for this edition.")
+        st.subheader(pdf_data[0], divider="rainbow")
+        with st.container(border=True):
+            if pdf_data:
+                streamlit_pdf_viewer.pdf_viewer(pdf_data[1], render_text=True)
+            else:
+                st.warning("⚠️ No PDF available for this edition.")
 
 if __name__ == "__main__":
+    if "admin" not in st.session_state:
+        st.session_state.admin = 0
     main()
